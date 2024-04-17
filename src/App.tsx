e970f55c8fa291740736eb8aea0b7f3a5269bd9a
Nakul <nakulyadav1710@gmail.com>
2024-04-17 23:36:17 +0530

    making todo list

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [todo,setTodo] = React.useState<any>([])
+  const [task,setTask] = React.useState<string>("") // task var setTask("nakul")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <input type="text"  onChange={(e:any)=>{
+        console.log(e.target.value);
+        setTask(e.target.value);
+      }} />
+      <button onClick={()=>{
+        let temp = todo;
+        temp.push(task);
+        setTodo(temp)
+      }}>Add</button>
+      </div>
+      <div>
+        <div>
+          {task}
+        </div>
+        <ul>
+          {todo.map((value:string)=>{
+            return(
+              <li>{value}</li>
+            )
+          })}
+         
+        </ul>
+      </div>
     </div>
   );
 }
