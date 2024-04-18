cdb65dcf89e0cf3e623954859981be57b6fafbca
Nakul <nakulyadav1710@gmail.com>
2024-04-18 13:34:48 +0530

    making todo list

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,65 @@ import './App.css';
 
 function App() {
   const [todo,setTodo] = React.useState<any>([])
+  const [dodo,setDodo] = React.useState<any>([])
   const [task,setTask] = React.useState<string>("") // task var setTask("nakul")
+  const inputChange = function (e:any){
+    setTask(e.target.value);
+    
+  }
+  // {
+  //   "task": "Task Name",
+  //   "status": "TODO/DONE",
+  //   "date" : "DATE"
+  //   }
+  const handleOnclick = function (){
+   console.log("click")
+   let temp = todo;
+   temp.push({
+    title: task,
+    status: "todo",
+
+   });
+   setTodo(temp);
+   setTask("");
+   console.log(task);
+  }
+  const handleDone = function (index:number){
+ console.log(index);
+ let temp = todo
+ temp[index].status = "done";
+ setTodo(temp);
+ setTask("");
+ let done = dodo;
+ done.push(temp[index]);
+ 
+  }
   return (
     <div className="App">
       <div>
-      <input type="text"  onChange={(e:any)=>{
-        console.log(e.target.value);
-        setTask(e.target.value);
-      }} />
-      <button onClick={()=>{
-        let temp = todo;
-        temp.push(task);
-        setTodo(temp)
-      }}>Add</button>
+      <input type="text" value={task}  onChange={inputChange} />
+      <button onClick={handleOnclick}>Add</button>
       </div>
       <div>
-        <div>
-          {task}
-        </div>
+  
         <ul>
-          {todo.map((value:string)=>{
+          {todo.map((value:any,index:number)=>{
             return(
-              <li>{value}</li>
+              <li key = {index}>{value.title} ({value.status}) <button onClick = {()=>{
+                handleDone(index)
+              }}>Done</button></li>
+              
+            )
+          })}
+         
+        </ul>
+        <ul>
+        {dodo.map((value:any,index:number)=>{
+            return(
+              <li key = {index}>{value.title} ({value.status}) <button onClick = {()=>{
+                handleDone(index)
+              }}>Done</button></li>
+              
             )
           })}
          
